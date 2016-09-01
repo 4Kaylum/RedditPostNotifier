ac2e7c47e9de90076f27c9de94e53e22a830cf67
CalebPC <coolum444@hotmail.co.uk>
2016-09-01 01:06:15 +0100

    Added comments to clear up what's going on, in general

--- mainBot.py
+++ mainBot.py
@@ -3,20 +3,29 @@ import OAuth2Util
 import time
 import os
 
+## Set some constants
 print('Opened.')
 emList = os.path.dirname(os.path.realpath(__file__)) + '\\emailList.txt'
 r = praw.Reddit(user_agent="A bot to email out when there's a new chapter to Magic Muggle")
 
+## Auth the bot to /u/magicSquib
 print('Logging in...')
 o = OAuth2Util.OAuth2Util(r)
 o.refresh(force=True)
 print('Logged in.\n')
 
+## Main event loop
 while True:
+
+	## Go through unread messages.
 	print('Going through messages...')
 	for msg in r.get_unread(limit=None):
+
+		## Print the subject to console.
 		print('Message subject line :: {}'.format(msg.subject))
 
+		## Identifier -- add
+		## Adds the user to the mail list
 		if msg.subject.lower().startswith("[add]"):
 			print('Identified - add to list.')
 			with open(emList) as a:
@@ -35,6 +44,9 @@ while True:
 				msg.reply("You've been added to the message list!\n\nIf you want to be removed, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[REMOVE]&message=User list user update.)")
 				print('Replied to user.')
 		
+
+		## Identifier -- remove
+		## Takes the user off of the mail list
 		elif msg.subject.lower().startswith("[remove]"):
 			print('Identified - remove from list.')
 			with open(emList) as a:
@@ -57,6 +69,10 @@ while True:
 				msg.reply("You're not on the message list! You silly :3\n\nIf you want to be added, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[ADD]&message=User list user update.)")
 				print('Replied to user.')
 
+
+		## Identifier -- send message
+		## Sends an message out to everyone on the message list
+		## You have to be SatanistSnowflake or Doomchicken7 to run
 		elif msg.subject.lower().startswith("[send message]") and str(msg.author) in ['SatanistSnowflake','Doomchicken7']:
 			with open(emList) as a:
 				print('Outward message send detected. Initiating...')
@@ -72,10 +88,12 @@ while True:
 					r.send_message(i,"New Magic Muggle chapter!","Check it out! \n\n/r/MagicMuggle")
 					print('    Sent!')
 
+		## Marks the message as read so it won't be read multiple times
 		print('Marking as read...')
 		msg.mark_as_read()
 		print('Marked.\n')
 
+	## Sleeps so the bot isn't timed out and banned by Reddit
 	print('Sleeping for 10 seconds...\n')
 	time.sleep(10)
 
