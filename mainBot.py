616a3327aba3e55fb6d419a83ae327bfed7ae8fe
Callum Bartlett <coolum444@hotmail.co.uk>
2016-09-03 17:33:43 +0100

    Massive efficiency changes New file, used with functions. Main bot file shortened massively, much cleaner, much smoother.

--- mainBot.py
+++ mainBot.py
@@ -2,6 +2,7 @@ import praw
 import OAuth2Util
 import time
 import os
+from postNotifierTools import *
 
 ## Set some constants
 print('Opened.')
@@ -14,87 +15,54 @@ o = OAuth2Util.OAuth2Util(r)
 o.refresh(force=True)
 print('Logged in.\n')
 
+justJoined    = "You've been added to the message list!\n\nIf you want to be removed, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[REMOVE]&message=User list user update.)"
+justLeft      = "You've been removed from the message list!\n\nIf you want to be added, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[ADD]&message=User list user update.)"
+alreadyJoined = "You're already on the message list! You silly :3\n\nIf you want to be removed, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[REMOVE]&message=User list user update.)"
+alreadyLeft   = "You're not on the message list! You silly :3\n\nIf you want to be added, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[ADD]&message=User list user update.)"
+
 ## Main event loop
 while True:
+
+	changeFlag = False
 
 	## Go through unread messages.
 	print('Going through messages...')
 	for msg in r.get_unread(limit=None):
 
 		## Print the subject to console.
-		print('Message subject line :: {}'.format(msg.subject))
+		print('\nMessage subject line :: {}'.format(msg.subject))
 
-		## Identifier -- add
-		## Adds the user to the mail list
-		if msg.subject.lower().startswith("[add]"):
-			print('Identified - add to list.')
-			with open(emList) as a:
-				peopleOnThere = a.read()
-				peopleOnThere = peopleOnThere.split('\n')
-				print(peopleOnThere)
-			
-			if msg.author.name in peopleOnThere:
-				print('Already on list.')
-				msg.reply("You're already on the message list! You silly :3\n\nIf you want to be removed, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[REMOVE]&message=User list user update.)")
-				print('Replied to user.')
+		## Get the users who are already on the message list
+		currentUsers = getFileList(emList)
+
+		## Checks to add to the list
+		if checkStartWith(msg, '[add]'):
+			print("    Identified :: ADD :: {}".format(msg.author.name))
+			if msg.author.name in currentUsers:
+				print("    Already on list. Aborting.")
+				msg.reply(alreadyJoined)
 			else:
-				with open(emList,'a') as a:
-					a.write('{}\n'.format(msg.author.name))
-				print('Added to list.')
-				msg.reply("You've been added to the message list!\n\nIf you want to be removed, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[REMOVE]&message=User list user update.)")
-				print('Replied to user.')
-		
+				print("    Not on list. Adding.")
+				changeFlag = True
+				currentUsers.append(msg.author.name)
+				msg.reply(justJoined)
 
-		## Identifier -- remove
-		## Takes the user off of the mail list
-		elif msg.subject.lower().startswith("[remove]"):
-			print('Identified - remove from list.')
-			with open(emList) as a:
-				peopleOnThere = a.read()
-				peopleOnThere = peopleOnThere.split('\n')
-				print(peopleOnThere)
+		## Checks to rem from the list
+		if checkStartWith(msg, '[remove]'):
+			print("    Identified :: REMOVE :: {}".format(msg.author.name))
+			if msg.author.name in currentUsers:
+				print("    Already on list. Removing.")
+				changeFlag = True
+				currentUsers.remove(msg.author.name)
+				msg.reply(justLeft)
+			else:
+				print("    Not on list. Aborting.")
+				msg.reply(alreadyLeft)
 
-			try:
-				peopleOnThere.remove(msg.author.name)
-				print('Removed from list.')
-				qwe = ''
-				for i in peopleOnThere:
-					qwe = qwe + i + '\n'
-				with open(emList,'w') as a:
-					a.write(qwe)
-				msg.reply("You've been removed from the message list!\n\nIf you want to be added, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[ADD]&message=User list user update.)")
-				print('Replied to user.')
-			except ValueError:
-				print('Not on list.')
-				msg.reply("You're not on the message list! You silly :3\n\nIf you want to be added, [click here!](https://www.reddit.com/message/compose/?to=magicSquib&subject=[ADD]&message=User list user update.)")
-				print('Replied to user.')
+	if changeFlag:
+		print("\nWriting changes to file.")
+		writeToFile(currentUsers, emList)
 
-
-		## Identifier -- send message
-		## Sends an message out to everyone on the message list
-		## You have to be SatanistSnowflake or Doomchicken7 to run
-		elif msg.subject.lower().startswith("[send message]") and str(msg.author) in ['SatanistSnowflake','Doomchicken7']:
-			with open(emList) as a:
-				print('Outward message send detected. Initiating...')
-				peopleOnThere = a.read()
-				peopleOnThere = peopleOnThere.split('\n')
-				print(peopleOnThere)
-
-			for i in peopleOnThere:
-				if i == '':
-					pass
-				else:
-					print('    Messaging {}...'.format(i))
-					r.send_message(i,"New Magic Muggle chapter!","Check it out! \n\n/r/MagicMuggle")
-					print('    Sent!')
-
-		## Marks the message as read so it won't be read multiple times
-		print('Marking as read...')
-		msg.mark_as_read()
-		print('Marked.\n')
-
-	## Sleeps so the bot isn't timed out and banned by Reddit
-	print('Sleeping for 30 seconds...\n')
+	print()
 	time.sleep(30)
 
-# r.send_message('user', 'Subject Line', 'You are awesome!')
